92374d120bfeec64f8a111845266d4aec58fb62e
Yan_jun <1977897092@qq.com>
2022-07-29 01:35:37 -0700

    second version

--- newfind.sh
+++ newfind.sh
@@ -2,26 +2,24 @@
 #
 if [ ! $1 ]
 then
-  echo "####### 请输入参数 #######"
+  echo "####### please input value #######"
   echo "$0 $1 is not an option"
   echo "$0 -a : find clk* "
   echo "$0 -b : find clk*.c"
   echo "$0 -c : git option"
   exit;
 fi
-while [ -n "$1" ]
-do
     case "$1" in
             #查找kernel目录下的以clk开头的文件
-        -a) echo "####### 开始查找 ####### ";
+        -a) echo "####### start find ####### ";
             find /home/dyj/Share/linux/kernel -name "clk*";
-            echo "####### 查找结束 ####### ";
+            echo "####### find over ####### ";
             exit;;
             
             #查找所有以clk开头的.c文件
-        -b) echo "####### 开始查找 ####### ";
+        -b) echo "####### start find ####### ";
             find /home/dyj/Share/linux/ -name "clk*"|grep ".c";
-            echo "####### 查找结束 ####### ";
+            echo "####### find over ####### ";
             exit;;
             
         -git)
@@ -31,9 +29,13 @@ do
             echo "$0 -b : find clk*.c";
             echo "$0 -c : git option";
             exit;;
+         *)
+           echo "$0 $1 is not an option";
+            echo "$0 -a : find clk* ";
+            echo "$0 -b : find clk*.c";
+            echo "$0 -c : git option";
+            exit;;
     esac
-    shift
-done
 if [ ! $2 ]
 then
     echo "please input file "
@@ -50,3 +52,30 @@ then
     exit;
 fi
 
+git add .
+
+git status
+
+#写个sleep 1s 是为了解决并发导致卡壳
+
+sleep 1s
+
+echo "####### 添加文件 #######"
+
+git commit -m "$3"
+
+echo "####### commit #######"
+
+sleep 1s
+
+echo "####### 开始推送 #######"
+
+if [ ! $4 ]
+then
+  echo "####### 请输入自己提交代码的分支 #######"
+  exit;
+fi
+
+git push origin "$4"
+
+echo "####### 推送成功 #######"
