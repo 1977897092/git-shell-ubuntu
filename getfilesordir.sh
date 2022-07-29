d41579688e021984856085a1fd36723681cbb3c6
Yan_jun <1977897092@qq.com>
2022-07-29 03:37:13 -0700

    new shell

---
+++ getfilesordir.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+getfilesordir(){
+    for file in `ls $1`
+    do
+        if test -f $file
+        then
+            echo "file:  $file"
+        elif test -d $file
+        then
+            echo "path: $file"
+        fi
+    done
+}
+path="./"
+getfilesordir $path
