bcb5d2616263304293e7a9a4e9b711a192816e75
Yan_jun <1977897092@qq.com>
2022-08-03 01:38:38 -0700

    add function to function_find.sh

--- function_find.sh
+++ function_find.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #将shell的功能作为函数封装起来
-help_information(){
+function help_information(){
   echo "####### please input value #######";
   echo "$0 $1 is not an option";
   echo "$0 -a : find clk* ";
@@ -8,45 +8,11 @@ help_information(){
   echo "$0 -c : find 'clk' in c files ";
   echo "$0 -help : help ";
   echo "$0 -git 'file' 'commit' ' branch ' : update your project to github";
+  echo "############## END ###############";
 }
 
-if [ ! $1 ]
-then
-  help_information;
-  exit;
-fi
-    case "$1" in
-            #查找kernel目录下的以clk开头的文件
-        -a) echo "####### start find ####### ";
-            find /home/dyj/Share/linux/ -name "clk*";
-            echo "####### find over ####### ";
-            exit;;
-            
-            #查找所有以clk开头的.c文件
-        -b) echo "####### start find ####### ";
-            find /home/dyj/Share/linux/ -name "clk*.c"
-            echo "####### find over ####### ";
-            exit;;
-            
-            #查找所有包含clk字符的.c文件
-        -c)find /home/dyj/Share/linux -name "*.c"|cat|while read line
-           do
-           grep -l clk $line
-           done      
-           exit;;
-           #查找包含clk字符的.c文件
-        -d)find /home/dyj/Share/linux -name "*.c"|xargs grep -l "clk";
-           exit;; 
-           #查找包含clk字符的文件
-        -e)grep -l -r "clk" /home/dyj/Share/linux;
-           exit;;
-           #查找包含clk的c文件
-        -f)grep -l -r "clk" /home/dyj/Share/linux --include="*.c";
-           exit;;
-           #-git 执行git代码提交指令
-        
-        -gitlocal)
-            if [ ! $2 ]
+function git_update_local(){
+if [ ! $2 ]
             then
             echo "please input file "
             exit;
@@ -63,11 +29,12 @@ fi
             fi
             git add $2;
             git status;
-            git commit -m  $3;:
-            exit;;
+            git commit -m  $3;
+}
 
-             -git)
-             if [ ! $2 ]
+function git_update_origin(){
+
+            if [ ! $2 ]
             then
                 echo "please input file "
             exit;
@@ -87,16 +54,46 @@ fi
             git status
             git commit -m "$3"
             git push origin "$4"
+
+}
+
+#查找kernel目录下的以clk开头的文件
+function find_clk_files(){
+            echo "####### start find ####### ";
+            find /home/dyj/Share/linux/ -name "clk*";
+            echo "####### find over ####### ";
+}
+
+#查找kernel目录下的包含clk字符的文件
+function find_clk_c_files(){
+            grep -l -r "clk" /home/dyj/Share/linux/ --include="*.c";
+}
+
+if [ ! $1 ]
+then
+  help_information;
+  exit;
+fi
+    case "$1" in
+          #查找kernel目录下的以clk开头的文件
+        -a)find_clk_files
             exit;;
-        -help) echo "$0 $1 is not an option";
-            echo "$0 -a : find clk* ";
-            echo "$0 -b : find clk*.c";
-            echo "$0 -c : git option";
+            
+        -b)find_clk_c_files
+           exit;;
+          
+           #-git 执行git代码提交指令
+        -gitlocal)
+            git_update_local
+            exit;;
+
+        -gitorigin)
+            git_update_origin
+            exit;;
+        -help)
+            help_information
             exit;;
          *)
-           echo "$0 $1 is not an option";
-            echo "$0 -a : find clk* ";
-            echo "$0 -b : find clk*.c";
-            echo "$0 -c : git option";
+            help_information
             exit;;
     esac
